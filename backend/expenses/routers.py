10bfe0d865e29fa74f3b1613be8b1b7eaa2e0fbd
andrea camargo <simbelmyne.evermind.131717@gmail.com>
2023-03-31 23:51:03 -0400

    backend: view endpoint port 3000, urls path,

---
+++ backend/expenses/routers.py
@@ -0,0 +1,16 @@
+from django.urls import path, include
+from rest_framework import routers
+# class to handle HTTP responses 
+from .views import ExpenseViewSet, IncomeViewSet, BudgetViewSet
+
+"""
+router to map the views to specific URLs
+"""
+router = routers.DefaultRouter()
+router.register(r'expenses', ExpenseViewSet)
+router.register(r'incomes', IncomeViewSet)
+router.register(r'budgets', BudgetViewSet)
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
