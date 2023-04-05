4763430fbea85011a8db3b5028e9e96d042e64ee
andrea camargo <simbelmyne.evermind.131717@gmail.com>
2023-04-05 15:57:48 -0400

    py_client list.py testind endpoind, also, new field description in model incomes and model expenses

--- backend/expenses/models.py
+++ backend/expenses/models.py
@@ -6,11 +6,13 @@ Models to represent the economic issues of a home, such as expenses, income, bud
 class Expense(models.Model):
     name = models.CharField(max_length=100)
     amount = models.DecimalField(max_digits=8, decimal_places=2)
+    description = models.TextField(blank=True, null=True)
     date = models.DateField()
 
 class Income(models.Model):
     name = models.CharField(max_length=100)
     amount = models.DecimalField(max_digits=8, decimal_places=2)
+    description = models.TextField(blank=True, null=True)
     date = models.DateField()
 
 class Budget(models.Model):
--- py_client/list.py
+++ py_client/list.py
@@ -1,6 +1,6 @@
 import requests
 
-endpoint = "http://127.0.0.1:3000/incomes/" 
+endpoint = "http://127.0.0.1:3000/budgets/" 
 
 get_response = requests.get(endpoint)
 print(get_response.json())
